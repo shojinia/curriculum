05812460590c9cff3a07676768344ea9296cb9aa
shojinia <105899389+shojinia@users.noreply.github.com>
2022-12-02 21:12:26 +0900

    課題提出
    2-11の課題提出

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_GET['my_name'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>私のメールアドレスは、<?php echo $email; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
