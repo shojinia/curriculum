20f23b548deaf7d35451b7bca4bb0ee59f3922c3
shojinia <105899389+shojinia@users.noreply.github.com>
2022-12-04 22:59:06 +0900

    課題提出
    ２−１２の課題提出

---
+++ 2-12/2-12.php
@@ -0,0 +1,20 @@
+<form action="result.php" method="post">
+    お名前：<input type="text" name="my_name" />
+    <br>
+    ご希望商品：
+        <input type="radio" name="gift" value="A賞">A賞
+        <input type="radio" name="gift" value="B賞">B賞
+        <input type="radio" name="gift" value="C賞">C賞
+    <br>
+    個数：
+    <select name="count">
+        <?php for ($i=1;$i<=10;$i++){ ?>
+            <option value="<?php echo $i; ?>">
+                <?php echo $i;?>
+            </option>
+        <?php }  ?>
+    </select>
+    <br>
+    <input type="submit" value="申込" />
+
+
---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+    $my_name = $_POST['my_name'];
+    $radio = $_POST['gift'];
+    $count = $_POST['count'];
+    ?>
+    <p>お名前：<?php echo $my_name; ?>です。</p>
+    <p>ご希望景品：<?php echo $radio; ?>です。</p>
+    <p>個数：<?php echo $selectct; ?>です。</p>
