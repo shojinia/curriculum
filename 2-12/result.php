da2b95aa96c65ffa7caa6104cb6f3bd3835b8cd6
shojinia <105899389+shojinia@users.noreply.github.com>
2022-12-12 22:05:12 +0900

    Update result.php
    再提出

--- 2-12/result.php
+++ 2-12/result.php
@@ -1,8 +1,8 @@
 <?php
     $my_name = $_POST['my_name'];
     $radio = $_POST['gift'];
-    $count = $_POST['count'];
+    $count = $_POST['number'];
     ?>
     <p>お名前：<?php echo $my_name; ?></p>
     <p>ご希望景品：<?php echo $radio; ?></p>
-    <p>個数：<?php echo $count; ?></p>
+    <p>個数：<?php echo $number; ?></p>
